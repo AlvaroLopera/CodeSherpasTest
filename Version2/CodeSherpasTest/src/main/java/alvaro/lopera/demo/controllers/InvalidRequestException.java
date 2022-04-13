66f900e77be900c0b2575c48b094fd0c43cfcb24
AlvaroLopera <103375672+AlvaroLopera@users.noreply.github.com>
2022-04-13 17:51:25 +0200

    Delete Version2/CodeSherpasTest directory

--- Version2/CodeSherpasTest/src/main/java/alvaro/lopera/demo/controllers/InvalidRequestException.java
+++
@@ -1,9 +0,0 @@
-package alvaro.lopera.demo.controllers;
-
-public class InvalidRequestException extends RuntimeException {
-    
-    public InvalidRequestException ( String msn ) {
-        super(msn);
-    }
-
-}
--- Version2/CodeSherpasTest/src/main/java/alvaro/lopera/demo/repositories/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package alvaro.lopera.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import alvaro.lopera.demo.entities.Client;
-
-public interface ClientRepository extends JpaRepository <Client, String> {
-    
-}
